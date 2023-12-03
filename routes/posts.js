168db89ca695fc74e85ca9db2e2f702ddf9660cb
cgoncalves94 <goncalves.cesaraugusto94@gmail.com>
2023-12-03 13:58:50 +0000

    Refactor postUtils module and update API routes

--- routes/posts.js
+++ routes/posts.js
@@ -7,11 +7,11 @@ const router = express.Router();
 // Importing Post model
 const Post = require('../models/Post');
 
-// Importing verify function
+// Importing verify token function
 const verifyToken = require('../verifyToken');
 
+// Importing the checkAndUpdatePostExpiration function from the postUtils module
 const checkAndUpdatePostExpiration = require('../utils/postUtils');
-
 
 
 
@@ -58,15 +58,14 @@ router.put('/:id/like', verifyToken, async (req, res) => {
 
         if (!post.likes.includes(req.user._id)) {
             await post.updateOne({ $push: { likes: req.user._id } });
-            res.status(200).json("The post has been liked");
+            res.status(200).json("The post has been liked"); // Send a success message
         } else {
-            res.status(400).json("You already liked this post");
+            res.status(400).json("You already liked this post"); // Send an error message if the user has already liked the post
         }
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
-
 
 // PUT /api/posts/:id/dislike - Dislike a post
 // This route handles the PUT request to dislike a post. It expects the request parameter to contain
@@ -90,12 +89,18 @@ router.put('/:id/dislike', verifyToken, async (req, res) => {
     }
 });
 
-
-
-
 // POST /api/posts/:id/comment - Add a comment to a post
-router.post('/:id/comment', async (req, res) => {
+// This route handles the POST request to add a comment to a post. It expects the request parameter to contain
+// the ID of the post and the request body to contain the comment text. The authenticated user's ID and the comment are added to the comments array of the post. 
+router.post('/:id/comment', verifyToken, async (req, res) => {
     try {
+        // Check if the post has expired
+        const { expired, message } = await checkAndUpdatePostExpiration(req.params.id);
+        if (expired) {
+            return res.status(400).json({ message });
+        }
+
+        // Proceed to add a comment if the post is not expired
         const comment = {
             user: req.user._id,
             comment: req.body.comment,
@@ -109,28 +114,44 @@ router.post('/:id/comment', async (req, res) => {
     }
 });
 
-
 // GET /api/posts/most-active/:topic - Get the most active post by topic
-router.get('/most-active/:topic', async (req, res) => {
+// This route handles the GET request to retrieve the most active post based on a specific topic.
+// It expects the request parameter to contain the topic.
+router.get('/most-active/:topic', verifyToken, async (req, res) => {
     try {
+        // Find all posts with the specified topic
         const posts = await Post.find({ topic: req.params.topic });
-        const mostActivePost = posts.sort((a, b) => (b.likes.length + b.dislikes.length) - (a.likes.length + a.dislikes.length))[0];
+
+        // Sort the posts based on the total number of likes and dislikes
+        const sortedPosts = posts.sort((a, b) => (b.likes.length + b.dislikes.length) - (a.likes.length + a.dislikes.length));
+
+        // Get the most active post, which is the first post in the sorted array
+        const mostActivePost = sortedPosts[0] || null; // Handle case where there are no posts
+
+        // Send the most active post as a JSON response
         res.json(mostActivePost);
     } catch (err) {
+        // Handle any errors by sending a JSON response with the error message
         res.status(500).json({ message: err.message });
     }
 });
+
 
 
 // GET /api/posts/expired/:topic - Get expired posts by topic
-router.get('/expired/:topic', async (req, res) => {
+// This route handles the GET request to retrieve expired posts based on a specific topic.
+// It expects the request parameter to contain the topic.
+router.get('/expired/:topic', verifyToken, async (req, res) => {
     try {
-        const expiredPosts = await Post.find({ topic: req.params.topic, expirationTime: { $lt: new Date() } });
+        // Fetch posts that have a status of 'Expired' and match the specified topic
+        const expiredPosts = await Post.find({ topic: req.params.topic, status: 'Expired' });
         res.json(expiredPosts);
     } catch (err) {
+        // Handle any errors by sending a JSON response with the error message
         res.status(500).json({ message: err.message });
     }
 });
+
 
 
 module.exports = router;
--- utils/postUtils.js
+++ utils/postUtils.js
@@ -1,25 +1,31 @@
-/**
- * Checks and updates the expiration status of a post.
- * @param {string} postId - The ID of the post to check.
- * @returns {Promise<{ expired: boolean, message?: string, post?: object }>} - An object indicating whether the post has expired, along with an optional message and the updated post object.
- */
+// Importing the required module
 const Post = require('../models/Post');
 
+// Defining an asynchronous function to check and update post expiration
 async function checkAndUpdatePostExpiration(postId) {
+    // Finding the post by its ID
     const post = await Post.findById(postId);
 
-    // Check if the post is already expired
+    // Checking if the post is already expired
     if (post.status === 'Expired') {
         return { expired: true, message: "This post has expired." };
     }
 
+    // Getting the current time
     const currentTime = new Date();
+
+    // Checking if the current time is greater than or equal to the post's expiration time
+    // and the post is not already expired
     if (currentTime >= post.expirationTime && post.status !== 'Expired') {
+        // Updating the post's status to 'Expired'
         post.status = 'Expired';
         await post.save();
         return { expired: true, message: "This post has expired." };
     }
 
+    // Returning the result indicating that the post is not expired
     return { expired: false, post };
 }
-module.exports = checkAndUpdatePostExpiration; // Exporting the function directly
+
+// Exporting the function directly
+module.exports = checkAndUpdatePostExpiration;
